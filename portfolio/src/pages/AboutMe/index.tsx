b4b64e86fd1db477bcd89f95af0833023cbd3968
Rodrigo Fernandes <fernandes.rodrigo.p1@gmail.com>
2022-08-22 17:39:09 -0300

    AboutMe Refactor ok

--- portfolio/src/pages/AboutMe/index.tsx
+++ portfolio/src/pages/AboutMe/index.tsx
@@ -1,30 +1,32 @@
 import styles from './AboutMe.module.scss';
-import fotoAtual from '../../assets/aboutMe/FotoAtual.jpg';
+import fotoAtual from '../../assets/aboutMe/FotoAtual2.jpg';
 import Button from '../../components/Button';
 
 export default function AboutMe () {
   return (
     <section className={styles.aboutMe}>
-      <div className={styles.aboutMe__TitleBox}>
-        <h3 className={styles.aboutMe__TitleBox___firstTitleComponent}>
-          Sobre Mim
-        </h3>
-        <h4 className={styles.aboutMe__TitleBox___secondTitle}>
-          Centrado e organizado.
-        </h4>
-        <p className={styles.aboutMe__TitleBox___content}>
-          Atualmente estou cursando 3° semestre em Análise e Desenvolvimento de
-          Sistemas e buscando uma oportunidade na área de tecnologia, venho
-          estudando a mais ou menos 2 anos com foco para me tornar um bom
-          programador Front end.
-        </p>
-        <p className={styles.aboutMe__TitleBox___content}>
-          Gosto de ler livros, praticar esportes, gastronomia, aprender e fazer
-          um bom trabalho.
-        </p>
-        <Button></Button>
+      <div className={styles.aboutMe__TitleBoxAndImg}>
+        <div className={styles.aboutMe__TitleBoxAndImg___titleBox}>
+          <h3 className={styles.aboutMe__TitleBoxAndImg___titleBox___firstTitleComponent}>
+            Sobre Mim
+          </h3>
+          <h4 className={styles.aboutMe__TitleBoxAndImg___titleBox___secondTitle}>
+            Centrado e organizado.
+          </h4>
+          <p className={styles.aboutMe__TitleBoxAndImg___titleBox___content}>
+            Atualmente estou cursando 3° semestre em Análise e Desenvolvimento de
+            Sistemas e buscando uma oportunidade na área de tecnologia, venho
+            estudando a mais ou menos 2 anos com foco para me tornar um bom
+            programador Front end.
+          </p>
+          <p className={styles.aboutMe__TitleBoxAndImg___titleBox___content}>
+            Gosto de ler livros, praticar esportes, gastronomia, aprender e fazer
+            um bom trabalho.
+          </p>
+          <Button></Button>
+        </div>
+        <img src={fotoAtual} className={styles.aboutMe__TitleBoxAndImg___img}/>
       </div>
-      <img src={fotoAtual} className={styles.aboutMe__img}/>
     </section>
   );
 }
