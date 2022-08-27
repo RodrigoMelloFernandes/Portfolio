06463a47260d856dae6514d225e68874f0401515
Rodrigo Fernandes <fernandes.rodrigo.p1@gmail.com>
2022-08-27 09:33:16 -0300

    Add Header Animations

--- portfolio/src/pages/Header/index.tsx
+++ portfolio/src/pages/Header/index.tsx
@@ -18,7 +18,7 @@ export default function Header () {
         type: 'tween',
         bounce: 0.4,
         duration: 1.5,
-        delay: 2.5,
+        delay: 1,
       },
     },
   };
@@ -33,7 +33,7 @@ export default function Header () {
         type: 'tween',
         bounce: 0.4,
         duration: 1.5,
-        delay: 3,
+        delay: 1.5,
       },
     },
   };
@@ -48,7 +48,7 @@ export default function Header () {
         type: 'tween',
         bounce: 0.4,
         duration: 2,
-        delay: 3,
+        delay: 2,
       },
     },
   };
