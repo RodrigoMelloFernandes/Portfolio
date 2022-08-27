dfe2867200fd2e0abe83af7ed1d74c82df7eb18e
Rodrigo Fernandes <fernandes.rodrigo.p1@gmail.com>
2022-08-27 10:19:11 -0300

    portfolios animation ok

--- portfolio/src/pages/Portfolios/PortfoliosBoxComponent/index.tsx
+++ portfolio/src/pages/Portfolios/PortfoliosBoxComponent/index.tsx
@@ -1,16 +1,37 @@
 import styles from './PortfoliosBoxComponent.module.scss';
-
+import { motion} from 'framer-motion';
 interface IPortfoliosImg {
   url : string
 }
 
 export default function portfoliosBoxComponent ({url}: IPortfoliosImg) {
+  const fadeInUpVariant = {
+    offscreen : {
+      y : 150,
+      opacity : 0
+    },
+    onscreen : {
+      y : 0,
+      opacity : 1,
+      transition : {
+        type : 'tween',
+        duration : 1,
+        delay: .2
+      }
+    }
+  };
+
   return (
-    <div className={styles.portfoliosBoxComponent}>
+    <motion.div className={styles.portfoliosBoxComponent}
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: true, amount: 0.8, margin : '400px'}}
+      variants={fadeInUpVariant}
+    >
       <img className={styles.portfoliosBoxComponent__img} src={url} alt="portfoliosImgs" />
       <div className={styles.portfoliosBoxComponent__boxComponentHover}>
         <h3 className={styles.portfoliosBoxComponent__boxComponentHover___title}>Em Breve</h3>
       </div>
-    </div>
+    </motion.div>
   );
 }
--- portfolio/src/pages/Portfolios/index.tsx
+++ portfolio/src/pages/Portfolios/index.tsx
@@ -1,11 +1,31 @@
 import styles from './Portfolios.module.scss';
 import portfoliosImgs from 'data/portfoliosImgs.json';
 import PortfoliosBoxComponent from './PortfoliosBoxComponent';
+import { motion } from 'framer-motion';
 
 export default function Portfolios () {
+  const bounceVariant = {
+    offscreen: {
+      scale : 0
+    },
+    onscreen : {
+      scale : [ .5 , 1],
+      transition : {
+        type : 'tween',
+        duration : 1,
+        delay : .3
+      }
+    }
+  };
+
   return (
     <section className={styles.portfolios}>
-      <h3 className={styles.portfolios__firstTitleComponent}>Portfólios</h3>
+      <motion.h3 className={styles.portfolios__firstTitleComponent}
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8, margin : '400px'}}
+        variants={bounceVariant}
+      >Portfólios</motion.h3>
       <div className={styles.portfolios__fatherBoxPortfoliosBoxComponent}>
         {
           portfoliosImgs.map((item, index) => (
