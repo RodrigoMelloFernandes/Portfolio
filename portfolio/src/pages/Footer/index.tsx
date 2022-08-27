9be1c5c9bde4bab47f544a21d5c5dcbfbf776d39
Rodrigo Fernandes <fernandes.rodrigo.p1@gmail.com>
2022-08-27 10:35:57 -0300

    Footer Animations Ok

--- portfolio/src/pages/Footer/index.tsx
+++ portfolio/src/pages/Footer/index.tsx
@@ -1,12 +1,86 @@
 import styles from './Footer.module.scss';
-
+import {motion} from 'framer-motion';
 import { FaInstagram, FaWhatsapp, FaLinkedinIn, FaGithub,} from 'react-icons/fa';
 
 export default function Footer() {
+  const bounceVariant = {
+    offscreen: {
+      scale : 0
+    },
+    onscreen : {
+      scale : [ .5 , 1],
+      transition : {
+        type : 'tween',
+        duration : 1,
+        delay : .3
+      }
+    }
+  };
+
+  const fadeInUpVariant = {
+    offscreen : {
+      y : 150,
+      opacity : 0
+    },
+    onscreen : {
+      y : 0,
+      opacity : 1,
+      transition : {
+        type : 'tween',
+        duration : 1,
+        delay: .2
+      }
+    }
+  };
+
+  const fadeInRightVariant = {
+    offscreen : {
+      x : 150,
+      opacity : 0
+    },
+    onscreen : {
+      x : 0,
+      opacity : 1,
+      transition : {
+        type : 'tween',
+        duration : 1,
+        delay: .2
+      }
+    }
+  };
+
+  const fadeInLeftVariant = {
+    offscreen : {
+      x : -150,
+      opacity : 0
+    },
+    onscreen : {
+      x : 0,
+      opacity : 1,
+      transition : {
+        type : 'tween',
+        duration : 1,
+        delay: .2
+      }
+    }
+  };
+
+  
+
   return (
     <footer className={styles.footer}>
-      <h2 className={styles.footer__title}>Contato</h2>
-      <form className={styles.footer__contactBox}>
+      <motion.h2 className={styles.footer__title}
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8, margin : '400px'}}
+        variants={bounceVariant}
+      >Contato</motion.h2>
+      <motion.form className={styles.footer__contactBox}
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8, margin : '400px'}}
+        variants={fadeInRightVariant}
+      >
         <input
           className={styles.footer__contactBox___input}
           type="text"
@@ -24,11 +98,21 @@ export default function Footer() {
         <button className={styles.footer__contactBox___button} type="submit">
           Enviar Mensagem
         </button>
-      </form>
+      </motion.form>
 
-      <hr className={styles.footer__line} />
+      <motion.hr className={styles.footer__line} 
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8, margin : '400px'}}
+        variants={fadeInLeftVariant}
+      />
       <div className={styles.footerSocialMBoxAndCopyRightBoxDesktopBox}>
-        <div className={styles.footer__footerSocialMBox}>
+        <motion.div className={styles.footer__footerSocialMBox}
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: true, amount: 0.8, margin : '400px'}}
+          variants={fadeInUpVariant}
+        >
           <div
             className={styles.footer__footerSocialMBox___footerSocialMBoxInBox}
           >
@@ -65,13 +149,18 @@ export default function Footer() {
               }
             />
           </div>
-        </div>
-        <div className={styles.footer__footerCopyRightBox}>
+        </motion.div>
+        <motion.div className={styles.footer__footerCopyRightBox}
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: true, amount: 0.8, margin : '400px'}}
+          variants={fadeInUpVariant}
+        >
           <p className={styles.footer__footerCopyRightBox__title}>
             © Desenvolvido por<span className={styles.footer__footerCopyRightBox__title___span}>              Rodrigo Fernandes
             </span>
           </p>
-        </div>
+        </motion.div>
       </div>
     </footer>
   );
